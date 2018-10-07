bb60e8491e03885f34179ef763f4c74570114700
Christophe DE BONI <christophe.de.boni@icloud.com>
2018-10-07 19:09:01 +0200

    2nd commit

--- Test du Source Control/ViewController.swift
+++ Test du Source Control/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("2nd commit commit")
+        print("Mon premier commit")
     }
 
     override func didReceiveMemoryWarning() {
